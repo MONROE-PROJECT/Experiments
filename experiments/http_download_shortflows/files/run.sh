ca0ab50b9714ae9c9294d38603d08c936c3fa597
jonakarl <jonas.karlsson@kau.se>
2016-04-15 14:04:08 +0200

    Removed debug flag

--- experiments/http_download_shortflows/files/run.sh
+++ experiments/http_download_shortflows/files/run.sh
@@ -15,4 +15,4 @@ SEGSIZE=$4
 SEGMENTS="1 3 8 11 19 32 64 91 128 182 236"
 URL="http://speedtest.bahnhof.net/1000M.zip"
 
-python curl_wrapper_shortflows.py --interface ${IF} --time ${MAX_TIME} --url ${URL} --interval ${INTERVAL} --segments ${SEGMENTS} --segmentsize ${SEGSIZE} --debug
+python curl_wrapper_shortflows.py --interface ${IF} --time ${MAX_TIME} --url ${URL} --interval ${INTERVAL} --segments ${SEGMENTS} --segmentsize ${SEGSIZE}
