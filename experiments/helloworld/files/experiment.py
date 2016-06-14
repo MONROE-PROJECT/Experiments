ab6745ceb443ac265662a74bb54ee4c55b7de005
Jonas Karlsson <jonas.karlsson@kau.se>
2016-06-14 11:40:35 +0200

    Can now be ran outside a monroe node (without metadata retrival)

--- experiments/helloworld/files/experiment.py
+++ experiments/helloworld/files/experiment.py
@@ -24,45 +24,54 @@ CONFIG = {
         "nr_of_messages": 3
         }
 
+print ("[{}] Hello: Default config {}").format(datetime.now(),
+                                               json.dumps(CONFIG,
+                                                          sort_keys=True,
+                                                          indent=2))
+
 try:
     with open(CONFIGFILE) as configfd:
         CONFIG.update(json.load(configfd))
+        # Attach to the ZeroMQ socket as a subscriber and start listen to
+        # MONROE messages
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        socket.connect(CONFIG['zmqport'])
+        socket.setsockopt(zmq.SUBSCRIBE, CONFIG['metadata_topic'])
+        # End Attach
+
+        # Parse X first messages
+        if CONFIG['verbosity'] > 1:
+            print ("[{}] Hello: Start recoding messages "
+                   "with configuration {}").format(datetime.now(),
+                                                   json.dumps(CONFIG,
+                                                              sort_keys=True,
+                                                              indent=2))
+        for x in xrange(CONFIG["nr_of_messages"]):
+
+            (topic, msgdata) = socket.recv().split(' ', 1)
+
+            msg = json.loads(msgdata)
+            if CONFIG['verbosity'] > 1:
+                    print ("[{}] Recieved message {}"
+                           " with topic : {}\n "
+                           "{}").format(datetime.now(), x,
+                                        topic,
+                                        json.dumps(msg,
+                                                   sort_keys=True,
+                                                   indent=2))
+
+            # Add some fields to the message
+            msg['NodeId'] = CONFIG['nodeid']
+            msg['Hello'] = "World"
+
+            # Save the message in the file with updated fields
+            monroe_exporter.save_output(msg, CONFIG['resultdir'])
+
 except Exception as e:
-    print "[{}] Cannot retrive config {}".format(datetime.now(), e)
-
-# Attach to the ZeroMQ socket as a subscriber and start listen to
-# MONROE messages
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect(CONFIG['zmqport'])
-socket.setsockopt(zmq.SUBSCRIBE, CONFIG['metadata_topic'])
-# End Attach
-
-# Parse X first messages
-if CONFIG['verbosity'] > 1:
-    print ("[{}] Hello: Start recoding messages "
-           "with configuration {}").format(datetime.now(),
-                                           json.dumps(CONFIG,
-                                                      sort_keys=True,
-                                                      indent=2))
-for x in xrange(CONFIG["nr_of_messages"]):
-
-    (topic, msgdata) = socket.recv().split(' ', 1)
-
-    msg = json.loads(msgdata)
-    if CONFIG['verbosity'] > 1:
-            print ("[{}] Recieved message {}"
-                   " with topic : {} \n"
-                   " {}").format(datetime.now(), x,
-                                 topic,
-                                 json.dumps(msg, sort_keys=True, indent=2))
-
-    # Add some fields to the message
-    msg['NodeId'] = CONFIG['nodeid']
-    msg['Hello'] = "World"
-
-    # Save the message in the file with updated fields
-    monroe_exporter.save_output(msg, CONFIG['resultdir'])
+    print ("[{}] Cannot retrive config {} "
+           "running outside a monre node?"
+           ", skip trying to get metdata").format(datetime.now(), e)
 
 if CONFIG['verbosity'] > 1:
-    print "[{}] Finished the experiment".format(datetime.now())
+    print "[{}] Hello : Finished the experiment".format(datetime.now())
