651a091d6b098ae660327a6b8cf299c6f9649b27
Jonas Karlsson <jonas.karlsson@kau.se>
2016-10-25 14:07:48 +0200

    Adde dmonroe/base build

---
+++ monroe_base/build.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+TMPNAME=monroe-base-local
+NAME=monroe/base
+
+docker pull debian:jessie && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} .
+ID=$(docker run -d ${TMPNAME} /bin/bash)
+docker export ${ID} | docker import - ${NAME} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
