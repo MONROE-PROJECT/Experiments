4527019b81c97fca62a89dbf5a2144869ce740ca
Mohammad Rajiullah <mohammad.rajiullah@kau.se>
2016-11-04 15:42:30 +0100

    update to headlessfirefox container

--- experiments/headlessfirefox/files/headless_browser.py
+++ experiments/headlessfirefox/files/headless_browser.py
@@ -187,8 +187,79 @@ def run_exp(meta_info, expconfig, url,count):
     har_stats["DataVersion"]= expconfig['dataversion']
     har_stats["NodeId"]= expconfig['nodeid']
     har_stats["Timestamp"]= time.time()
-    har_stats["Iccid"]= meta_info["ICCID"]
-    har_stats["Operator"]= meta_info["Operator"]
+    try:
+    	har_stats["Iccid"]= meta_info["ICCID"]
+    except Exception:
+    	print("ICCID info is not available")
+    try:
+    	har_stats["Operator"]= meta_info["Operator"]
+    except Exception:
+    	print("Operator info is not available")
+    try:
+    	har_stats["IMSI"]=meta_info["IMSI"]
+    except Exception:
+    	print("IMSI info is not available")
+    try:
+    	har_stats["IMEI"]=meta_info["IMEI"]
+    except Exception:
+    	print("IMEI info is not available")
+    try:
+    	har_stats["InternalInterface"]=meta_info["InternalInterface"]
+    except Exception:
+    	print("InternalInterface info is not available")
+    try:
+    	har_stats["IPAddress"]=meta_info["IPAddress"]
+    except Exception:
+    	print("IPAddress info is not available")
+    try:
+    	har_stats["InternalIPAddress"]=meta_info["InternalIPAddress"]
+    except Exception:
+    	print("InternalIPAddress info is not available")
+    try:
+    	har_stats["InterfaceName"]=meta_info["InterfaceName"]
+    except Exception:
+    	print("InterfaceName info is not available")
+    try:
+    	har_stats["IMSIMCCMNC"]=meta_info["IMSIMCCMNC"]
+    except Exception:
+    	print("IMSIMCCMNC info is not available")
+    try:
+    	har_stats["NWMCCMNC"]=meta_info["NWMCCMNC"]
+    except Exception:
+    	print("NWMCCMNC info is not available")
+    try:
+    	har_stats["LAC"]=meta_info["LAC"]
+    except Exception:
+    	print("LAC info is not available")
+    try:
+    	har_stats["CID"]=meta_info["CID"]
+    except Exception:
+    	print("CID info is not available")
+    try:
+    	har_stats["RSCP"]=meta_info["RSCP"]
+    except Exception:
+    	print("RSCP info is not available")
+    try:
+    	har_stats["RSSI"]=meta_info["RSSI"]
+    except Exception:
+    	print("RSSI info is not available")
+    try:
+    	har_stats["ECIO"]=meta_info["ECIO"]
+    except Exception:
+    	print("ECIO info is not available")
+    try:
+    	har_stats["DeviceMode"]=meta_info["DeviceMode"]
+    except Exception:
+    	print("DeviceMode info is not available")
+    try:
+    	har_stats["DeviceSubmode"]=meta_info["DeviceSubmode"]
+    except Exception:
+    	print("DeviceSubmode info is not available")
+    try:
+    	har_stats["DeviceState"]=meta_info["DeviceState"]
+    except Exception:
+    	print("DeviceState info is not available")
+
     har_stats["SequenceNumber"]= count
 
     #msg=json.dumps(har_stats)
