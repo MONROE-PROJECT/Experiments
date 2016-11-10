46e9c3120fca5e5fe3938fb3ff19eb988d0f3b2e
Jonas Karlsson <jonas.karlsson@kau.se>
2016-11-10 16:47:27 +0100

    Almost there
    Most find a way to parse the private key and test with real data from scheduler

---
+++ tunnelbox/tunnelbox-server/files/monroe-sshtunnel-server.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+source /etc/profile
+[[ -z  ${USER}  ]] && echo "No USER exiting" && exit
+[[ -z  ${SERVERPORT}  ]] && echo "No server port exiting" && exit
+[[ -z  ${SCHEDULERURL}  ]] && echo "No scheduler url exiting" && exit
+[[ -z  ${UPDATEFREQ}  ]] && echo "No key update frequencey defined exiting" && exit
+[[ ! -f ${CERT} ]] && echo "No tunnel-server cert/pem exiting" && exit
+[[ ! -f ${KEY} ]] && echo "No tunnel-server key exiting" && exit
+[[ ! -d "/home/${USER}/.ssh" ]] && echo "No user home directory (.ssh)" && exit
+
+AUTHKEY_FILE="/home/${USER}/.ssh/authorized_keys"
+CURL_OPT="--key ${KEY} --cert ${CERT} --insecure ${SCHEDULERURL}"
+JQ_OPT="--raw-output .[]"
+echo "Starting sshd"
+/usr/sbin/sshd -p ${SERVERPORT}
+
+# Print some diagnostics
+echo "#############################"
+echo "USER : ${USER}"
+echo "SSHPORT : ${SERVERPORT}"
+echo "Scheduler URL : ${SCHEDULERURL}"
+echo "Delay between pulls : ${UPDATEFREQ}"
+echo -n "Cert used : "
+cat ${CERT}
+echo ""
+echo -n "Key used : "
+cat ${KEY}
+echo ""
+echo "Key retrival command command : curl ${CURL_OPT} | jq ${JQ_OPT} > ${AUTHKEY_FILE}"
+
+
+#Start tunnel and loop forever
+while true
+do
+  #curl ${CURL_OPT} | jq ${JQ_OPT} > ${AUTHKEY_FILE}
+  cat /opt/monroe/curlresults.txt | jq ${JQ_OPT} > ${AUTHKEY_FILE}
+  echo "Authorized keys : "
+  cat ${AUTHKEY_FILE}
+  echo "Connections:"
+  netstat -tnpa | grep 'ESTABLISHED.*sshd'
+  sleep ${UPDATEFREQ}
+done
+echo " SSH tunnel-server finished"
--- tunnelbox/tunnelbox-testclient/build.sh
+++ tunnelbox/tunnelbox-testclient/build.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONTAINER=${DIR##*/}
 DOCKERFILE=${CONTAINER}.docker
 
-docker pull debian:jessie
+docker pull monroe/base
 docker build --no-cache --rm -f ${DOCKERFILE} -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
