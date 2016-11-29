c4dd0ad2f9819445ec7ca1348f964cbe4768045d
Jonas Karlsson <jonas.karlsson@kau.se>
2016-11-29 15:33:19 +0100

    Adding missing build.sh file and modified push.sh to point to correct repo

--- tunnelbox-server/push.sh
+++ tunnelbox-server/push.sh
@@ -4,6 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=${DIR##*/}
 
-CONTAINERTAG=monroe/${CONTAINER}
+REPO=monroe1.cs.kau.se:5000
+CONTAINERTAG=${REPO}/monroe/${CONTAINER}
 
-docker login && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
+#CONTAINERTAG=monroe/${CONTAINER}
+
+docker login ${REPO} && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
