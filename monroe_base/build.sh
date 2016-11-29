e8b58f114a59ff2022af134e4f418058922a6e45
Jonas Karlsson <jonas.karlsson@kau.se>
2016-11-29 14:11:19 +0100

    Added support for firefox headless

--- monroe_base/build.sh
+++ monroe_base/build.sh
@@ -3,6 +3,6 @@
 TMPNAME=monroe-base-local
 NAME=monroe/base
 
-docker pull debian:jessie && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} .
-ID=$(docker run -d ${TMPNAME} /bin/bash)
-docker export ${ID} | docker import - ${NAME} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
+docker pull debian:jessie && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} . \
+&& ID=$(docker run -d ${TMPNAME} /bin/bash) \
+&& docker export ${ID} | docker import - ${NAME} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
