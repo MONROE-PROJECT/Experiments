6c5e35bfdbba73c4e4a0deb2385500fdeedfe8e9
moharaji <moharaji@kau.se>
2016-12-04 01:36:02 +0100

    update to headless browsers

--- experiments/headlessbrowsing/files/start.sh
+++ experiments/headlessbrowsing/files/start.sh
@@ -6,12 +6,13 @@ export PATH=$PATH:/opt/monroe/
 
 echo "Experiment starts ..."
 
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "$line" > one_url
-    python headless_browser.py one_url 1 h1
-    python headless_browser.py one_url 1 h2
-    python headless_browser.py one_url 1 h1s
-done < url
+python headless_browser_with_user_input.py
+#while IFS='' read -r line || [[ -n "$line" ]]; do
+#    echo "$line" > one_url
+#    python headless_browser.py one_url 1 h1
+#    python headless_browser.py one_url 1 h2
+#    python headless_browser.py one_url 1 h1s
+#done < url
 
 tar -zcvf /monroe/results/results$RANDOM.tgz /tmp/*.json
 #python headless_browser.py one_url 10 h1
