6c9bb3258204524cbb7428f8559462b15b5187db
Mohammad Rajiullah <moharaji@kau.se>
2017-02-07 16:58:07 +0100

    Removed obsolete experiment headlessfirefox

--- experiments/headlessfirefox/push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-CONTAINER=${DIR##*/}
-
-CONTAINERTAG=mraj/http2measurements # Modify to your own dockerhub user/repo
-
-docker login && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
