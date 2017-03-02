2ad3f26275c00ee70cfdc4aa95b0b06687d047f3
Jonas Karlsson <jonas.karlsson@kau.se>
2017-03-02 11:08:09 +0100

    New build system, all new changes are pushed to dockeru hub monroe/base:staging Offical monreo/base (monroe/base) will be updated at regular interval after testing of regressions in staging)
    New in staging:
    New version of Firefox and Gecko driver
    Added missing dnsutils

--- monroe_base/build.sh
+++ monroe_base/build.sh
@@ -2,7 +2,8 @@
 
 TMPNAME=monroe-base-local
 NAME=monroe/base
+TAG=staging
 
 docker pull debian:jessie && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} . \
 && ID=$(docker run -d ${TMPNAME} /bin/bash) \
-&& docker export ${ID} | docker import - ${NAME} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
+&& docker export ${ID} | docker import - ${NAME}:${TAG} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
--- monroe_base/push.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-docker login && docker push monroe/base && echo "Now rebuild all images that are built on monroe/base"
