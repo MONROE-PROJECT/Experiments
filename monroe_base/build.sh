8acba9a71d951f7d3e905e544d18e9894f407fac
Jonas Karlsson <jonas.karlsson@kau.se>
2017-08-25 14:02:49 +0200

    monroe/base is now based on debian:stretch
    Changes (besides upgrades due to stretch):
    
     * python3-scapy
     * firefox 52.3.0esr
     * Gecko Driver 0.18.0
     * Flent is now in debian offical repository
     * dig and bind recompiled to support new library versions
    
    The new version is compiled and avaliabe as monroe/base:staging and will be pushed to monroe/base(:latest) when verification process is finished.
    
    Old version of monroe/base will only be avaliabe as monroe/base:jessie after :staging has been pushed to :latest
    
    Initial testinmg has been performed on this version but specifically headless firefox/selenium/gecko integration needs evaluatiuon before final verification.

--- monroe_base/build.sh
+++ monroe_base/build.sh
@@ -4,6 +4,6 @@ TMPNAME=monroe-base-local
 NAME=monroe/base
 TAG=staging
 
-docker pull debian:jessie && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} . \
+docker pull debian:stretch && docker build --rm --no-cache -f monroe_base.docker -t ${TMPNAME} . \
 && ID=$(docker run -d ${TMPNAME} /bin/bash) \
 && docker export ${ID} | docker import - ${NAME}:${TAG} && docker rm -f ${ID} && docker rmi -f ${TMPNAME} && docker images -a |grep "${NAME} "
