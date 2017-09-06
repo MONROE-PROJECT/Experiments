b5490d5ec609aa6c552d8a59da60a494f4cac038
Jonas Karlsson <jonas.karlsson@kau.se>
2017-09-06 08:17:31 +0200

    Update push-official.sh
    Rearranged script for easier reading

--- monroe_base/push-official.sh
+++ monroe_base/push-official.sh
@@ -1,5 +1,6 @@
 #/bin/sh
 TODAY=$(date +"%y%m%d")
 docker tag monore/base:staging monore/base
-docker tag monore/base:staging monore/base:${TODAY}
-docker login && docker push monroe/base && echo "Pushed official monroe/base:{TODAY}"
+docker login && docker push monroe/base && echo "Pushed official monroe/base:latest"
+docker tag monore/base monore/base:${TODAY}
+docker push monore/base:${TODAY} && echo "Pushed official monroe/base:${TODAY}"
