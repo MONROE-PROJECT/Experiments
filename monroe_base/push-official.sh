2b86d2016aad7f82d1f02b07ed1152ae81692b59
Jonas Karlsson <jonas.karlsson@kau.se>
2017-09-19 10:16:46 +0200

    Added correct repor and removed ambiquity

--- monroe_base/push-official.sh
+++ monroe_base/push-official.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 TODAY=$(date +"%y%m%d")
-docker tag monore/base:staging monore/base
-docker tag monore/base:staging monore/base:${TODAY}
-docker login && docker push monroe/base && echo "Pushed official monroe/base:{TODAY}"
+docker pull monroe/base:staging
+docker tag monroe/base:staging monroe/base:${TODAY}
+docker login && \
+docker push monroe/base:${TODAY} && echo "Pushed official monroe/base:{TODAY}" && \
+docker tag monroe/base:${TODAY} monroe/base:latest && \
+docker push monroe/base && echo "Pushed official monroe/base(:latest)"
