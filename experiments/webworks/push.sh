6cac577f39ef0245b1408be9cfadf84b8aeb05e5
Jonas Karlsson <jonas.karlsson@kau.se>
2017-10-04 14:21:39 +0200

    Changed structure to allow easy build and upload

--- experiments/webworks/push.sh
+++ experiments/webworks/push.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=${DIR##*/}
-
-CONTAINERTAG=mraj/http2measurements # Modify to your own dockerhub user/repo
+REPO=docker.monroe-system.eu
+CONTAINERTAG=docker.monroe-system.eu/monroe/monroe-web/image # Modify to your own dockerhub user/repo
 
 docker login ${REPO} && docker tag ${CONTAINER} ${CONTAINERTAG} && docker push ${CONTAINERTAG} && echo "Finished uploading ${CONTAINERTAG}"
