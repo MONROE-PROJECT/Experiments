7de5e402bb857789623804b4017e741e8255c70d
Your Name <you@example.com>
2017-11-30 14:45:34 +0000

    Using ramdisks when building on node

--- virtualization/build-node.sh
+++ virtualization/build-node.sh
@@ -2,19 +2,36 @@
 
 image=monroe/base:virtualization
 
-filesystem_image="image.tar"
+ram_disk_path=/tmp/vmramdisk
+filesystem_image="${ram_disk_path}/image.tar"
 disk_image="image.qcow2"
-# We make the disk_image 500 MB larger than necessary
+# We make the disk_image 100 MB larger than necessary
 # Will be allocated on demand
-disk_size="+500M"
+disk_size="+100M"
+max_image_size="1000"
 
 echo "$(date): Pulling the image"
 docker pull ${image}
 
+image_size=$(docker images --format "{{.Size}}" ${image}| tr -dc '0-9.'|cut -f1 -d ".") 
+
+if (( $image_size > $max_image_size ));then 
+	echo "Too big image $image_size"
+	exit 1 
+fi
+
 echo "$(date): Running a container from '${image}' image"
 container_id=$(docker run -d --net=none ${image} ls)
 
+ram_disk_size=$((image_size + 100))
+echo "Creating ${ram_disk_size}Mb ramdisk in ${ram_disk_path}"
+mkdir -p ${ram_disk_path}
+mount -t tmpfs -o size=${ram_disk_size}m tmpfs ${ram_disk_path}
+ 
+
 echo "$(date): Exporting image content to a tar archive"
+#doable but slowert due to compression
+#docker export ${container_id}  | gzip > ${ram_disk_path}/${filesystem_image}.gz
 docker export ${container_id} > ${filesystem_image}
 
 echo "$(date): Creating new QCOW2 disk image"
@@ -24,6 +41,9 @@ virt-make-fs \
   --type=ext4 \
   --partition -- ${filesystem_image} ${disk_image}
 
-echo "$(date): Deleting intermediate image"
-rm -f $filesystem_image
+echo "$(date): Unmounting ramdisk"
+rm -f ${filesystem_image}
+umount ${ram_disk_path}
+sleep 3
+rm -rf ${ram_disk_path}
 
