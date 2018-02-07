cb888b63e83549b1ddfd60f88aca5793e6f094ff
Mohammad Rajiullah <moharaji@kau.se>
2018-02-07 13:05:34 +0100

    cleaned up

--- experiments/monroe-browsertime/files/browsertime-master-old/browserscripts/pageinfo/documentWidth.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  function docWidth(doc) {
-    var body = doc.body, docelem = doc.documentElement;
-    return Math.max(body.scrollWidth, body.offsetWidth, docelem.clientWidth, docelem.scrollWidth, docelem.offsetWidth);
-  }
-
-  return docWidth(document);
-})();
--- experiments/monroe-browsertime/files/browsertime-master-old/test/prepostscripts/postSample.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    run(context) {
-      context.log.info('In posttask!!! (with results: ' + JSON.stringify(context.results) + ')');
-    }
-  };
