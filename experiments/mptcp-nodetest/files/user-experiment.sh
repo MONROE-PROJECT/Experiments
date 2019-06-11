440a12afc569321abfb802734ff271dacbb1a916
Jonas Karlsson <jonas.karlsson@kau.se>
2019-06-11 14:21:56 +0200

    Update user-experiment.sh

--- experiments/mptcp-nodetest/files/user-experiment.sh
+++ experiments/mptcp-nodetest/files/user-experiment.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "Starting curl mptcp test" >> /outdir/mptcp.check
 curl http://www.multipath-tcp.org >> /outdir/mptcp.check
+echo "Finished mptcp test, powering off" >> /outdir/mptcp.check
+poweroff
