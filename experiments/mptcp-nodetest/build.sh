c8f7a7fe748e5b240251ae14785e227b78c708db
Jonas Karlsson <jonas.karlsson@kau.se>
2019-06-18 09:42:45 +0200

    Use prebuilt mptcp kernel

--- experiments/mptcp-nodetest/build.sh
+++ experiments/mptcp-nodetest/build.sh
@@ -3,5 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 CONTAINER=mptcp-nodetest
 
+echo "Login to docker.cs.kau.se"
+docker login docker.cs.kau.se
+echo "Pull mptcp container"
+docker pull docker.cs.kau.se/monroe/containers/mptcp:build
+
 docker pull $(awk '/FROM/{ print $2 }' Dockerfile | head -n1)
 docker build --rm --no-cache -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
