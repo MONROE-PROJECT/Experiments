e23b3b92c252eca5eb4026325aaa24a4aa8c5a5e
Jonas Karlsson <jonas.karlsson@kau.se>
2021-02-09 15:50:55 +0100

    Added compiling dashc
    Working:
    - runs in ubuntu18.04 (the build container)
        - Can play :
            - http://www.cs.ucc.ie/~jq5/www_dataset_temp/x264_4sec/bbb_10min/DASH_Files/VOD/bbb_enc_10min_x264_dash.mpd
            - https://raw.githubusercontent.com/uccmisl/dashc/master/test_mpd/tearsofsteel_enc_x264_dash.mpd
            - http://www.cs.ucc.ie/~jq5/www_dataset_temp/x265_10sec/sstb_16min/DASH_Files/VOD/sstb_enc_16min_x265_dash.mpd
        - Cannot play (crash as the formast of the file is different): https://panoplay.duckdns.org/abs/stream/out.mpd
    - untested in monroe/base

---
+++ experiments/dash_c/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CONTAINER=${DIR##*/}
+
+docker pull $(awk '/FROM/{ print $2 }' Dockerfile | head -n1)
+#docker build --rm --no-cache -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
+docker build --rm -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
