bf553939227f6ce9b397b4f1d94612f5b3786b98
Mohammad Rajiullah <mohammad.rajiullah@kau.se>
2021-02-26 12:15:09 +0100

    Cleaned up the container

--- experiments/browsertime-dec20/build.sh
+++ experiments/browsertime-dec20/build.sh
@@ -4,5 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONTAINER=${DIR##*/}
 
 docker pull $(awk '/FROM/{ print $2 }' Dockerfile)
-#docker build   -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
-docker build --rm --no-cache -t ${CONTAINER} . && echo "Finished building ${CONTAINER}"
+docker build --rm --no-cache -t temp . && echo "Finished building temp"
+ID=$(docker run -d --entrypoint bash temp)
+docker export ${ID} | docker import - test
+docker build -t ${CONTAINER} -f Dockerfile-sq .
+docker rm -f ${ID}
+docker rmi temp
+docker rmi test
